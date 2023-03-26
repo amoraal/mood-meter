22bd8190fe521a8ff4138d1b671012be42ea2d63
Alexander Moraal <468709@student.saxion.nl>
2023-03-26 03:07:47 +0200

    initial commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Mood Meter
+This is a simple web application that allows users to track their emotions using a mood meter chart. The application takes two inputs from the user: energy level and pleasantness level, and plots the results on the mood meter chart. The mood meter chart is a simple two-axis graph that allows users to visualize their emotions in terms of energy and pleasantness.
+
+## 👇What is a mood meter?
+A mood meter is a tool that can be used to help people identify and understand their emotions. It typically consists of a chart or diagram that divides emotions into different categories, such as "happy," "sad," "angry," or "anxious," and allows individuals to plot their current emotional state on the chart.
+
+## ⭐Features
+
+- Display your current mood by rating your energy and pleasantness levels
+- Provide a description of the word that describes your mood
+- View the entire mood chart
+
+## 🚀 Getting started
+The provided project is written in pure HTML, CSS and JavaScript and should be able to run on any webserver. If not: ¯\\_(ツ)_/¯
+
---
+++ static/js/moodmeter.js
@@ -0,0 +1,95 @@
+const matrix = [
+    ["Despair", "Hopeless", "Desolate", "Spent", "Drained", "Sleepy", "Complacent", "Tranquil", "Cozy", "Serene"],
+    ["Despondent", "Depressed", "Sullen", "Exhausted", "Fatigued", "Mellow", "Thoughtful", "Peaceful", "Comfy", "Carefree"],
+    ["Alienated", "Miserable", "Lonely", "Disheartened", "Tired", "Relaxed", "Chill", "Restful", "Blessed", "Balanced"],
+    ["Pessimistic", "Morose", "Discouraged", "Sad", "Bored", "Calm", "Secure", "Satisfied", "Grateful", "Touched"],
+    ["Disgusted", "Glum", "Disappointed", "Down", "Apathic", "At Ease", " Easygoing", "Content", "Loving", "Fulfilled"],
+    ["Repulsed", "Troubled", "Concerned", "Uneasy", "Peeved", "Pleasant", "Joyful", "Hopeful", "Playful", "Blissful"],
+    ["Anxious", "Apprehensive", "Worried", "Irritated", "Annoyed", "Pleased", "Happy", "Focused", "Proud", "Thrilled"],
+    ["Fuming", "Frightened", "Angry", "Nervous", "Restless", "Energized", "Lively", "Enthusiastic", "Optimistic", "Excited"],
+    ["Livid", "Furious", "Frustrated", "Tense", "Stunned", "Hyper", "Cheerful", "Motivated", "Inspired", "Elated"],
+    ["Enraged", "Panicked", "Stressed", "Jittery", "Shocked", "Surprised", "Upbeat", "Festive", "Exhilirated", "Ecstatic"]
+]
+
+for(var i = matrix.length - 1; i >= 0; i--) {
+    const newDiv = document.createElement("div");
+    let matrixClassName = "matrix-row-" + i
+    newDiv.id = matrixClassName
+    document.getElementById('moodmatrix').appendChild(newDiv);
+    for(j in matrix[i]) {
+        var div = document.createElement("div");
+        div.className = "matrix-box"
+        div.innerHTML = matrix[i][j];
+
+        if(i <= 4 && j <= 4) {
+            div.style.background = "#0054ff";
+            div.style.color = "#ffffff";
+        } else if(i <= 4 && j >= 4) {
+            div.style.background = "#7ee728";
+        } else if(i > 4 && j <= 4 ) {
+            div.style.background = "#cd2f2f";
+            div.style.color = "#ffffff";
+        } else if(i > 4 && j > 4) {
+            div.style.background = "#FFEA00";
+        }
+
+        document.getElementById(matrixClassName).appendChild(div);
+    }
+}
+
+const openMatrix = document.querySelector('#open-matrix');
+openMatrix.addEventListener('click', (e) => {
+    document.getElementById("result").style.display = 'none';
+    document.getElementById("moodmeter").style.display = 'block';
+})
+
+const btn = document.querySelector('#submit');
+btn.addEventListener('click', (e) => {
+    const energy = document.getElementById('energy').value
+    const pleasantness = document.getElementById('pleasantness').value
+
+    document.getElementById("question").style.display = 'none';
+    document.getElementById("result").style.display = 'block';
+
+    var moodtext = document.getElementById("moodtext");
+    var meaning = document.getElementById("meaning")
+    let word = matrix[energy][pleasantness]
+    moodtext.textContent += word;
+
+    foo(meaning, word.toLowerCase());
+
+    if(energy <= 4 && pleasantness <= 4) {
+        document.getElementById("mood").style.backgroundColor = "#0054ff";
+    } else if(energy <= 4 && pleasantness >= 4) {
+        document.getElementById("mood").style.backgroundColor = "#7ee728";
+    } else if(energy > 4 && pleasantness <= 4 ) {
+        document.getElementById("mood").style.backgroundColor = "#cd2f2f";
+    } else if(energy > 4 && pleasantness > 4) {
+        document.getElementById("mood").style.backgroundColor = "#FFEA00";
+    }
+});
+
+
+async function foo(parent, word) {
+    let obj;
+    const res = await fetch('./dictionary.json')
+    obj = await res.json();
+
+    var partOfSpeech = document.createElement('div')
+    partOfSpeech.className = "partOfSpeech"
+    partOfSpeech.innerHTML = obj[word][0].partOfSpeech;
+    parent.appendChild(partOfSpeech);
+
+    var definition = document.createElement('span')
+    definition.className = "definition"
+    definition.innerHTML = obj[word][0].definition;
+    parent.appendChild(definition);
+
+    if (obj[word][0].example) {
+        var example = document.createElement('span')
+        example.className = "example"
+        example.innerHTML = obj[word][0].example;
+        parent.appendChild(example);
+    }
+
+}
